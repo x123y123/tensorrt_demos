338a9c403f4c199d37d309a9ed0f3d96dc97578c
tony <a0921444212@gmail.com>
2023-05-24 17:28:06 +0800

    add kalman part

---
+++ kalman.py
@@ -0,0 +1,56 @@
+import numpy as np
+import time
+
+import filterpy.kalman as kf
+from dronekit import connect, VehicleMode,LocationGlobal,LocationGlobalRelative
+from pymavlink import mavutil
+
+class MyKalman:
+    def __init__(self) -> None:
+        #self.f = kf(dim_x = 1, dim_z = 1)
+        self.x = np.zeros(1)
+        self.F = np.array([1.])
+        self.H = np.array([1.])
+        self.P = 4.73
+        self.R = 2.258578818
+        self.Q = 0.;
+
+    def bbox2dist(self, bbox) -> float:
+        n = 3.180226752
+        l0 = 1903.809012
+
+        return (n * (l0 / (bbox ** 0.5) - 1))
+
+    def dist2bbox(self, dist) -> float:
+        n = 3.180226752
+        l0 = 1903.809012
+
+        return (l0 * (dist / n + 1) ** -1) ** 2
+
+    def update(self, dist, vel, dt: float) -> float:
+        B = np.array([dt])
+        u = np.array([vel])
+        z = np.array([dist])
+
+        self.x, self.P = kf.predict(self.x, self.P, u = u, B = dt)
+        self.x, self.P = kf.update(self.x, self.P, z, self.R, self.H)
+
+        return self.x[0]
+
+    def connect_px4(self) -> None:
+        self.vehicle = connect('/dev/ttyACM0', wait_ready=True)
+
+    def disconnect_px4(self) -> None:
+        self.vehicle.close()
+
+if __name__ == '__main__':
+    kf = MyKalman()
+    kf.connect_px4()
+    time.sleep(2)
+    #for i in range(20):
+    while(True):
+        print("gps: %s\n" % kf.vehicle.gps_0)
+        print("G Location: %s\n" % kf.vehicle.location.global_relative_frame)
+        print("L Location: %s\n" % kf.vehicle.location.local_frame)
+        print("==========================================\n")
+    kf.disconnect_px4()
--- vision_core.py
+++ vision_core.py
@@ -18,10 +18,9 @@ from utils.display import open_window, set_display, show_fps
 from utils.visualization import BBoxVisualization
 from utils.yolo_with_plugins import TrtYOLO
 from control import drone_controller
-
+from kalman import MyKalman
 
 WINDOW_NAME = 'TrtYOLODemo'
-
 
 def parse_args():
     """Parse input arguments."""
@@ -50,7 +49,7 @@ def parse_args():
     return args
 
 
-def loop_and_detect(cam, trt_yolo, conf_th, vis, writer, img_width, img_height):
+def loop_and_detect(cam, trt_yolo, conf_th, vis,img_width,img_height):
     """Continuously capture images from camera and do object detection.
     # Arguments
       cam: the camera instance (video source).
@@ -58,14 +57,21 @@ def loop_and_detect(cam, trt_yolo, conf_th, vis, writer, img_width, img_height):
       conf_th: confidence/score threshold for object detection.
       vis: for visualization.
     """
+    kalman_start = time.time()
     connection_string = '/dev/ttyACM0'
-    #drone = drone_controller(connection_string)
+    sleep_time = 0.5
+    kf = MyKalman()
+    drone = drone_controller(connection_string)
     #drone.takeoff(1.25)
 
+    frame_cnt = 0
     full_scrn = False
     fps = 0.0
     tic = time.time()
+    cnt = 0
+#    while frame_cnt < 100:
     while True:
+        frame_start = time.time()
         """
         if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
             break
@@ -75,65 +81,114 @@ def loop_and_detect(cam, trt_yolo, conf_th, vis, writer, img_width, img_height):
             break
         boxes, confs, clss = trt_yolo.detect(img, conf_th)
         img , bb_coor = vis.draw_bboxes(img, boxes, confs, clss)
-        writer.write(img)
+        
         
         if (len(boxes) > 0):
-            print("control ready!")
-            
+            kalman_Y = time.time()
+            dt = kalman_Y - kalman_start
+            kalman_start = kalman_Y
+
+            Area = 0 
             # calculate bbox center
             bb_center_w = (bb_coor[2] - bb_coor[0]) / 2 + bb_coor[0]
             bb_center_h = (bb_coor[3] - bb_coor[1]) / 2 + bb_coor[1]
             Area = (bb_coor[2] - bb_coor[0]) * (bb_coor[3] - bb_coor[1])
+            #Area = Area / 100000
             print(f"Area: {Area}")
+            t = open("/home/uav/code/tensorrt_demos/bb_dis_25.txt", "a+")
+            t.write("25 " + str(Area))
+            t.write("\n")
+            
+            # kalman filter
+            dist = kf.bbox2dist(Area)
+            dist_E = kf.update(dist, drone.vehicle.velocity[0], dt)
+            kal = open("/home/uav/code/tensorrt_demos/kal_dist_test2.txt", "a+")
+            kal.write(str(dist) + " " + str(dist_E) + "\n")
+            kal.close()
+            
             # calculate img center
             img_center_w = img_width / 2
             img_center_h = img_height / 2
-            print(f"bb_center_w: {bb_center_w}")
-            print(f"bb_center_h: {bb_center_h}")
-            print(f"img_center_w: {img_center_w}")
-            print(f"img_center_h: {img_center_h}")
+            #print(f"bb_center_w: {bb_center_w}")
+            #print(f"bb_center_h: {bb_center_h}")
+            #print(f"img_center_w: {img_center_w}")
+            #print(f"img_center_h: {img_center_h}")
             
             if ((bb_center_h - img_center_h) != 0):
+                h_index = abs(bb_center_h - img_center_h)
+                #print(f"height_index: {h_index}")
                 if (bb_center_h < img_center_h):
-                    print("move up!")
-                    #drone.move_up()
-                    time.sleep(0.5)
+                    #print("move up!")
+                    z = -0.05
 
                 elif (bb_center_h > img_center_h):
-                    print("move down!")
-                    #drone.move_down()
-                    time.sleep(0.5)
+                    #print("move down!")
+                    z = 0.05
+            else:
+                z = 0
 
             if ((bb_center_w - img_center_w) != 0):
+                w_index = abs(bb_center_w - img_center_w)
+                #print(f"width_index: {w_index}")
                 if (bb_center_w < img_center_w):
-                    print("move left!")
-                    #drone.move_left()
-                    time.sleep(0.5)
+                    #print("move left!")
+                    y = -0.05
 
                 elif (bb_center_w > img_center_w):
-                    print("move right")
-                    #drone.move_right()
-                    time.sleep(0.5)
+                    #print("move right")
+                    y = 0.05
+            else:
+                y = 0
 
-            if (Area > 5000):
-                #drone.move_backward()
-                time.sleep(0.5)
-
-            elif (Area < 3000):
-                #drone.move_forward()
-                time.sleep(0.5)
+            depth = 0
+            if (h_index < 400 and w_index < 400):
+                if (Area > 7):
+                    x = -0.05
+                    
+                    #drone.move_backward()
+                    f = open("/home/uav/code/tensorrt_demos/depth.txt", "r")
+                    depth_str = f.read()
+                    f.close()
+                    try:
+                        depth = int(depth_str)
+                    except ValueError:
+                        print("some_variable did not contain a number!")
+                    if (depth < 30):
+                        print("[stereo] move back")
+                        x = -0.05
+                    else:
+                        print("\033[32m [stereo] \033[0m move forward")
+                        x = 0.05
+                    
+                    
+                elif (Area < 7):
+                    #drone.move_forward()
+                    x = 0.05
         else:
-            print("No object detect")
+            print("\033[31m No object detect \033[0m")
+            x = 0
+            y = 0
+            z = 0
+        
+        if (cnt == 5):
+            #drone.move(x, y, z)
+            #time.sleep(sleep_time)
+            cnt = 0
+            #print("move")
+        cnt = cnt + 1
+        #writer.write(img)
+        frame_end = time.time()
+        frame_time = frame_end - frame_start
+        #print(f"per frame:{frame_time}")
 
-        
-        
-        """
+        """ 
         img = show_fps(img, fps)
         cv2.imshow(WINDOW_NAME, img)
         toc = time.time()
         curr_fps = 1.0 / (toc - tic)
         # calculate an exponentially decaying average of fps number
-        fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+        #fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+        fps = curr_fps
         tic = toc             
         key = cv2.waitKey(1)
         if key == 27:  # ESC key: quit program
@@ -142,13 +197,17 @@ def loop_and_detect(cam, trt_yolo, conf_th, vis, writer, img_width, img_height):
             full_scrn = not full_scrn
             set_display(WINDOW_NAME, full_scrn)
         """
+        frame_cnt = frame_cnt + 1 
+        
 
 def main():
+    
     args = parse_args()
     if args.category_num <= 0:
         raise SystemExit('ERROR: bad category_num (%d)!' % args.category_num)
     if not os.path.isfile('yolo/%s.trt' % args.model):
         raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % args.model)
+    
 
     cam = Camera(args)
     if not cam.isOpened():
@@ -164,21 +223,15 @@ def main():
         	'Camera TensorRT YOLO Demo',
         	cam.img_width, cam.img_height
         	)
-    """ 
-    writer = cv2.VideoWriter(
-    				#args.output,
-    				'result.mp4',
-    				cv2.VideoWriter_fourcc(*'mp4v'), 
-    				30, 
-    				(cam.img_width, cam.img_height)
-    			      )
-    loop_and_detect(cam, trt_yolo, conf_th=0.3, vis=vis, writer=writer, img_width=cam.img_width, img_height=cam.img_height)
-
-    writer.release()
+    """
+    loop_and_detect(cam,trt_yolo,args.conf_thresh,vis=vis, img_width = cam.img_width, img_height = cam.img_height)
+    #writer.release()
     cam.release()
-    """
-    cv2.destroyAllWindows()
-    """
+    #distance.join()
+    print('Done.')
+    
+    #cv2.destroyAllWindows()
+    
 
 if __name__ == '__main__':
     main()
