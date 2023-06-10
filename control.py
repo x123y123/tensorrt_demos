229dea606aa154f1c582f1609ae5aa93e4bf229f
tony <a0921444212@gmail.com>
2023-06-10 16:30:40 +0800

    modify vision control part

--- control.py
+++ control.py
@@ -42,6 +42,7 @@ class drone_controller:
         return: None
         """
         self.vehicle.mode = VehicleMode("GUIDED")
+        #self.vehicle.mode = VehicleMode("LOITER")
         
         self.vehicle.armed = True
 
@@ -280,7 +281,74 @@ class drone_controller:
         end_time = time.time()
         latency = end_time - start_time
         print(f"move_right latency: {latency}")
+"""
+def setPositionTarget(vehicle: Vehicle, position: Tuple[float, float], yawRate=0) -> None:
+    global IS_LOITER
 
+    localNorth, localEast = position
+
+    if localNorth == 0 and localEast == 0:
+        # Loiter in place with guided mode
+        setLoiterGuided(vehicle, yawRate)
+    else:
+        IS_LOITER = False
+
+        # Find altitude target for NED frame
+        currentAltitude = vehicle.location.global_relative_frame.alt
+        targetAltOffset = 0.0 - (ALTITUDE - currentAltitude) # up is negative
+
+        ignoreVelocityMask =  0b111000
+        ignoreAccelMask =  0b111000000
+        ignoreYaw = 0b10000000000
+        emptyMask = 0b0000000000000000
+
+        msg = vehicle.message_factory.set_position_target_local_ned_encode(
+            0,       # time_boot_ms (not used)
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, # Use offset from current position
+            emptyMask + ignoreAccelMask + ignoreVelocityMask + ignoreYaw, # type_mask
+            localNorth, localEast, targetAltOffset,
+            0, 0, 0, # x, y, z velocity in m/s (not used)
+            0, 0, 0, # x, y, z acceleration (not used)
+            0, math.radians(yawRate))    # yaw, yaw_rate
+
+        vehicle.send_mavlink(msg)
+
+def setLoiterGuided(vehicle: Vehicle, yawRate: float) -> None:
+    global IS_LOITER
+    global LOITER_POSITION
+
+    if IS_LOITER != True:
+        IS_LOITER = True
+
+        # update position
+        frame = vehicle.location.global_relative_frame
+        LOITER_POSITION = {
+            "latitude": frame.lat,
+            "longitude": frame.lon
+        }
+
+    ignoreVelocityMask =  0b111000
+    ignoreAccelMask =  0b111000000
+    ignoreYaw = 0b10000000000
+    emptyMask = 0b0000000000000000
+
+    msg = vehicle.message_factory.set_position_target_global_int_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        emptyMask + ignoreAccelMask + ignoreVelocityMask + ignoreYaw, # type_mask (only speeds enabled)
+        int(LOITER_POSITION["latitude"] * 1e7), # lat_int - X Position in WGS84 frame in 1e7 * meters
+        int(LOITER_POSITION["longitude"] * 1e7), # lon_int - Y Position in WGS84 frame in 1e7 * meters
+        ALTITUDE,
+        0, # X velocity in NED frame in m/s
+        0, # Y velocity in NED frame in m/s
+        0, # Z velocity in NED frame in m/s
+        0, 0, 0, # afx, afy, afz acceleration
+        0, math.radians(yawRate))    # yaw, yaw_rate (rad/s)
+
+    vehicle.send_mavlink(msg)
+"""
 ############MAIN###############
 if __name__ == '__main__':
     connection_string = '/dev/ttyACM0'
--- vision_core.py
+++ vision_core.py
@@ -4,9 +4,12 @@ This script demonstrates how to do real-time object detection with
 TensorRT optimized YOLO engine.
 """
 
+
 import os
 import time
 import argparse
+import numpy as np
+import math
 
 import cv2
 import pycuda.autoinit  # This is needed for initializing CUDA driver
@@ -20,6 +23,8 @@ from control import drone_controller
 from kalman import MyKalman
 
 WINDOW_NAME = 'TrtYOLODemo'
+VIDEO_STREAM = False
+OPEN_KALMAN = True
 
 def parse_args():
     """Parse input arguments."""
@@ -48,7 +53,7 @@ def parse_args():
     return args
 
 
-def loop_and_detect(cam, trt_yolo, conf_th, vis,img_width,img_height):
+def loop_and_detect(cam, trt_yolo, conf_th, writer, vis,img_width,img_height):
     """Continuously capture images from camera and do object detection.
     # Arguments
       cam: the camera instance (video source).
@@ -56,89 +61,195 @@ def loop_and_detect(cam, trt_yolo, conf_th, vis,img_width,img_height):
       conf_th: confidence/score threshold for object detection.
       vis: for visualization.
     """
-    kalman_start = time.time()
+    
+    if (OPEN_KALMAN):
+        kalman_start = time.time()
+        kf = MyKalman(mode = 1)
+    
     connection_string = '/dev/ttyACM0'
-    sleep_time = 0.5
-    kf = MyKalman()
+    sleep_time = 0.1
     drone = drone_controller(connection_string)
-    #drone.takeoff(1.25)
-
+    drone.takeoff(0.5)
     frame_cnt = 0
+    detect_cnt = 0
     full_scrn = False
     fps = 0.0
     tic = time.time()
     cnt = 0
-#    while frame_cnt < 100:
+    threshold = 400
+
+    #while detect_cnt < 100:
+    #while frame_cnt < 100:
     while True:
         frame_start = time.time()
-        """
-        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
-            break
-        """
+        
+        if (VIDEO_STREAM):
+            if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+                break
+        
         img = cam.read()
         if img is None:
             break
+        print("img work")
         boxes, confs, clss = trt_yolo.detect(img, conf_th)
         img , bb_coor = vis.draw_bboxes(img, boxes, confs, clss)
-        
-        
+        Area = 0 
+        dist = 0
         if (len(boxes) > 0):
-            kalman_Y = time.time()
-            dt = kalman_Y - kalman_start
-            kalman_start = kalman_Y
+            detect_cnt += 1
 
-            Area = 0 
             # calculate bbox center
             bb_center_w = (bb_coor[2] - bb_coor[0]) / 2 + bb_coor[0]
             bb_center_h = (bb_coor[3] - bb_coor[1]) / 2 + bb_coor[1]
             Area = (bb_coor[2] - bb_coor[0]) * (bb_coor[3] - bb_coor[1])
             #Area = Area / 100000
             print(f"Area: {Area}")
-            t = open("/home/uav/code/tensorrt_demos/bb_dis_25.txt", "a+")
-            t.write("25 " + str(Area))
-            t.write("\n")
-            
+       
             # kalman filter
-            dist = kf.bbox2dist(Area)
-            dist_E = kf.update(dist, drone.vehicle.velocity[0], dt)
-            kal = open("/home/uav/code/tensorrt_demos/kal_dist_test2.txt", "a+")
-            kal.write(str(dist) + " " + str(dist_E) + "\n")
-            kal.close()
-            
+            if (OPEN_KALMAN):
+                #calc dt
+                kalman_Y = time.time()
+                dt = kalman_Y - kalman_start
+                kalman_start = kalman_Y
+                
+                #test 3D kalman
+                vel = np.array([drone.vehicle.velocity[0], drone.vehicle.velocity[1]])
+                yaw = drone.vehicle.attitude.yaw * math.pi / 180
+                data = kf.update(Area, vel, dt, yaw = yaw)
+                lat = drone.vehicle.location.global_frame.lat
+                lon = drone.vehicle.location.global_frame.lon
+                kf_save = open("/home/uav/code/tensorrt_demos/kf_data_0309_1.txt", "a+")
+                kf_save.write(str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[2]) + ' ' + str(lat) + ' ' + str(lon) + '\n')
+                kf_save.close()
+                print(str(data[0]) + ' ' + str(data[1]) + ' ' + str(data[2]) + ' ' + str(lat) + ' ' + str(lon) + ' ' + str(yaw) + '\n')
+
+                #put area into kalman
+                dist = round(kf.bbox2dist(Area), 2)
+                text = "kalman dist: " + str(dist)
+                cv2.putText(
+                            img, 
+                            text, 
+                            (0, 320), 
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, 
+                            (0, 255, 255), 
+                            1, 
+                            cv2.LINE_AA
+                        )
+              
             # calculate img center
             img_center_w = img_width / 2
             img_center_h = img_height / 2
-            #print(f"bb_center_w: {bb_center_w}")
-            #print(f"bb_center_h: {bb_center_h}")
-            #print(f"img_center_w: {img_center_w}")
-            #print(f"img_center_h: {img_center_h}")
-            
-            if ((bb_center_h - img_center_h) != 0):
-                h_index = abs(bb_center_h - img_center_h)
-                #print(f"height_index: {h_index}")
+            #draw threshold
+            cv2.circle(img, (320, 240), 20, (255, 0, 0), 2)
+
+            if ((bb_center_h - img_center_h)**2 + (bb_center_w - img_center_w)**2) > threshold:
+                x = 0
                 if (bb_center_h < img_center_h):
-                    #print("move up!")
+                    cv2.putText(
+                            img, 
+                            "move up", 
+                            (0, 360), 
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, 
+                            (0, 255, 255), 
+                            1, 
+                            cv2.LINE_AA
+                            )
+                    print("move up!")
                     z = -0.05
 
                 elif (bb_center_h > img_center_h):
-                    #print("move down!")
+                    cv2.putText(
+                            img, 
+                            "move down", 
+                            (0, 360), 
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, 
+                            (0, 255, 255), 
+                            1, 
+                            cv2.LINE_AA
+                            )
+                    print("move down!")
                     z = 0.05
-            else:
-                z = 0
 
-            if ((bb_center_w - img_center_w) != 0):
-                w_index = abs(bb_center_w - img_center_w)
-                #print(f"width_index: {w_index}")
                 if (bb_center_w < img_center_w):
-                    #print("move left!")
+                    cv2.putText(
+                            img, 
+                            "move left", 
+                            (0, 400), 
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, 
+                            (0, 255, 255), 
+                            1, 
+                            cv2.LINE_AA
+                            )
+                    print("move left!")
                     y = -0.05
 
                 elif (bb_center_w > img_center_w):
-                    #print("move right")
+                    cv2.putText(
+                            img, 
+                            "move right", 
+                            (0, 400), 
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, 
+                            (0, 255, 255), 
+                            1, 
+                            cv2.LINE_AA
+                            )
+                    print("move right")
                     y = 0.05
             else:
+                cv2.putText(
+                            img, 
+                            "move forward", 
+                            (0, 440), 
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, 
+                            (0, 255, 255), 
+                            1, 
+                            cv2.LINE_AA
+                        )
+                print("move forward")
+                x = 0.05
                 y = 0
+                z = 0
+        
+            if (OPEN_KALMAN):
+                if (dist < 25):     # modify 25~20
+                    cv2.putText(
+                                img, 
+                                "move backward", 
+                                (0, 440), 
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                1, 
+                                (0, 255, 255), 
+                                1, 
+                                cv2.LINE_AA
+                            )
+                    print("move back")
+                    x = -0.15
+                    y = 0
+                    z = 0
 
+            else:
+                if (Area > 55000):
+                    cv2.putText(
+                                img, 
+                                "move backward", 
+                                (0, 440), 
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                1, 
+                                (0, 255, 255), 
+                                1, 
+                                cv2.LINE_AA
+                            )
+                    print("move back")
+                    x = -0.15
+                    y = 0
+                    z = 0
+            """
             depth = 0
             if (h_index < 400 and w_index < 400):
                 if (Area > 7):
@@ -163,39 +274,69 @@ def loop_and_detect(cam, trt_yolo, conf_th, vis,img_width,img_height):
                 elif (Area < 7):
                     #drone.move_forward()
                     x = 0.05
+            """
         else:
             print("\033[31m No object detect \033[0m")
             x = 0
             y = 0
             z = 0
-        
+        """
         if (cnt == 5):
-            #drone.move(x, y, z)
+            drone.move(x, y, z)
             #time.sleep(sleep_time)
             cnt = 0
             #print("move")
+        """
+        
+        v_value = "[" + str(x) + ", " + str(y) + ", " + str(z) + "]"
+        cv2.putText(
+                    img, 
+                    v_value, 
+                    (0, 280), 
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1, 
+                    (0, 255, 255), 
+                    1, 
+                    cv2.LINE_AA
+                    )
+        drone.move(x, y, z)
+        time.sleep(sleep_time)
         cnt = cnt + 1
-        #writer.write(img)
+        writer.write(img)
         frame_end = time.time()
         frame_time = frame_end - frame_start
-        #print(f"per frame:{frame_time}")
+        print(f"per frame:{frame_time}")
+        #t = open("/home/uav/code/tensorrt_demos/tony/dvfs_data/static_power/object/1core.txt", "a+")
+        #t.write(str(frame_cnt) + " " + str(frame_time))
+        #t.write("\n")
+        #c = open("/sys/bus/i2c/drivers/ina3221/7-0040/hwmon/hwmon5/curr2_input", "r")
+        #current = int(c.read())
+        #c.close()
+        #v = open("/sys/bus/i2c/drivers/ina3221/7-0040/hwmon/hwmon5/in2_input", "r")
+        #voltage = int(v.read())
+        #v.close()
+        #power = current * voltage / 1000000
+        #w = open("/home/uav/code/tensorrt_demos/tony/dvfs_data/static_power/nonobject/power.txt", "a+")
+        #w.write(str(frame_cnt) + " " + str(power))
+        #w.write("\n")
 
-        """ 
-        img = show_fps(img, fps)
-        cv2.imshow(WINDOW_NAME, img)
-        toc = time.time()
-        curr_fps = 1.0 / (toc - tic)
-        # calculate an exponentially decaying average of fps number
-        #fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
-        fps = curr_fps
-        tic = toc             
-        key = cv2.waitKey(1)
-        if key == 27:  # ESC key: quit program
-            break
-        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
-            full_scrn = not full_scrn
-            set_display(WINDOW_NAME, full_scrn)
-        """
+        
+        if (VIDEO_STREAM):
+            img = show_fps(img, fps)
+            cv2.imshow(WINDOW_NAME, img)
+            toc = time.time()
+            curr_fps = 1.0 / (toc - tic)
+            # calculate an exponentially decaying average of fps number
+            #fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+            fps = curr_fps
+            tic = toc             
+            key = cv2.waitKey(1)
+            if key == 27:  # ESC key: quit program
+                break
+            elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
+                full_scrn = not full_scrn
+                set_display(WINDOW_NAME, full_scrn)
+        
         frame_cnt = frame_cnt + 1 
         
 
@@ -212,24 +353,43 @@ def main():
     if not cam.isOpened():
         raise SystemExit('ERROR: failed to open camera!')
 
+    frame_width = 640
+    frame_height = 480
+
+    writer = cv2.VideoWriter(
+                '/home/uav/code/tensorrt_demos/result.mp4',
+                cv2.VideoWriter_fourcc(*'mp4v'),
+                10,
+                (frame_width, frame_height),
+                )
+
+
     cls_dict = get_cls_dict(args.category_num)
     vis = BBoxVisualization(cls_dict)
     trt_yolo = TrtYOLO(args.model, args.category_num, args.letter_box)
-
-    """
-    open_window(
-        	WINDOW_NAME, 
-        	'Camera TensorRT YOLO Demo',
-        	cam.img_width, cam.img_height
-        	)
-    """
-    loop_and_detect(cam,trt_yolo,args.conf_thresh,vis=vis, img_width = cam.img_width, img_height = cam.img_height)
-    #writer.release()
+    
+    if (VIDEO_STREAM):
+        open_window(
+            	WINDOW_NAME, 
+            	'Camera TensorRT YOLO Demo',
+            	cam.img_width, cam.img_height
+            	)
+    
+    loop_and_detect(
+            cam,trt_yolo,
+            args.conf_thresh,
+            writer = writer,
+            vis=vis, 
+            img_width = cam.img_width, 
+            img_height = cam.img_height
+            )
+    writer.release()
     cam.release()
     #distance.join()
     print('Done.')
     
-    #cv2.destroyAllWindows()
+    if (VIDEO_STREAM):
+        cv2.destroyAllWindows()
     
 
 if __name__ == '__main__':
